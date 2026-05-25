ceb74ada29678f526795c586ef4b1c1a28c756a0
akash2003git <akash.tayade.dev@gmail.com>
2026-05-25 13:18:59 +0530

    azure env vars setup

--- WMS.API/Program.cs
+++ WMS.API/Program.cs
@@ -159,11 +159,14 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseGlobalExceptionMiddleware();
 app.UseCors("AllowAngular");
---
+++ WMS.Frontend/src/environments/environment.prod.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  apiUrl: 'https://wms-api-akash-efhzb7dfd4dkcfah.centralindia-01.azurewebsites.net/api'
+};
